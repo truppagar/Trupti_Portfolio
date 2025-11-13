1804e3d380fbeb7e7a1599d2366fe6f5b59266f9
Truptii <truptipagar1112@gmail.com>
2025-11-13 12:10:21 -0500

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,134 @@
+/*
+Author       : theme_ocean
+Template Name: Anushka - Personal Portfolio Template
+Version      : 1.0
+*/
+(function($) {
+	'use strict';
+	
+	jQuery(document).on('ready', function(){
+	
+		/*PRELOADER JS*/
+		$(window).on('load', function() { 
+			$('.status').fadeOut();
+			$('.preloader').delay(350).fadeOut('slow'); 
+		}); 
+		/*END PRELOADER JS*/	
+			
+		/*START MENU JS*/				
+		$('#main-menu').slicknav({
+			label: '',
+			duration: 1000,
+			easingOpen: "easeOutBounce", //available with jQuery UI
+			prependTo:'#mobile_menu',
+			closeOnClick: true,
+			easingClose:"swing", 
+			easingOpen:"swing", 
+			openedSymbol: "&#9660;",
+			closedSymbol: "&#9658;" 	
+		});
+		
+		if ($(window).scrollTop() > 200) {
+              $('.fixed-top').addClass('menu-bg');
+          } else {
+              $('.fixed-top').removeClass('menu-bg');
+          }
+			$(window).on('scroll', function(){
+				if ( $(window).scrollTop() > 70 ) {
+					$('.site-navigation, .header-white, .header').addClass('navbar-fixed');
+				} else {
+					$('.site-navigation, .header-white, .header').removeClass('navbar-fixed');
+				}
+			});		  	
+		/*END MENU JS*/
+
+		/*START PROGRESS BAR*/
+	    $('.progress-bar > span').each(function(){
+			var $this = $(this);
+			var width = $(this).data('percent');
+			$this.css({
+				'transition' : 'width 2s'
+			});
+			
+			setTimeout(function() {
+				$this.appear(function() {
+						$this.css('width', width + '%');
+				});
+			}, 500);
+		});
+		/*END PROGRESS BAR*/	
+
+		/* START COUNTDOWN JS*/
+		$('.counter_feature').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+			if (visible) {
+				$(this).find('.counter-num').each(function () {
+					var $this = $(this);
+					$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+						duration: 2000,
+						easing: 'swing',
+						step: function () {
+							$this.text(Math.ceil(this.Counter));
+						}
+					});
+				});
+				$(this).unbind('inview');
+			}
+		});
+		/* END COUNTDOWN JS */		
+
+		/* START JQUERY LIGHTBOX*/
+		jQuery('.lightbox').venobox({
+			numeratio: true,
+			infinigall: true
+		});	
+		/* END JQUERY LIGHTBOX*/	
+
+		/* START MIX JS */
+		$('.portfolio_item').mixItUp({
+		
+		});	
+
+		/*START VIDEO JS*/
+		$('.video-play').magnificPopup({
+            type: 'iframe'
+        });
+		/*END VIDEO JS*/		
+			
+	}); 
+
+		/*START TESTIMONIAL JS*/	
+		 $("#testimonial-slider").owlCarousel({
+			items:1,
+			itemsDesktop:[1000,1],
+			itemsDesktopSmall:[979,1],
+			itemsTablet:[768,1],
+			itemsMobile:[650,1],
+			pagination:true,
+			autoPlay:false
+		});		
+		/*END TESTIMONIAL JS*/		
+
+	/* START PARALLAX JS */
+	(function () {
+
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		 
+		} else {
+			$(window).stellar({
+				horizontalScrolling: false,
+				responsive: true
+			});
+		}
+
+	}());
+	/* END PARALLAX JS  */
+	
+	/*START WOW ANIMATION JS*/
+	  new WOW().init();	
+	/*END WOW ANIMATION JS*/	
+			
+})(jQuery);
+
+
+  
+
